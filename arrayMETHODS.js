5e6c4356a3bd8b19e1c4ec69dfb035d9b0be3ba3
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-02-19 20:00:31 +0545

    Create arrayMETHODS.js

---
+++ arrayMETHODS.js
@@ -0,0 +1,22 @@
+//Array methods
+
+let num =[1, 2, 3, 34, 4]
+let b= num.toString()
+console.log(b)
+//join method
+let c= num.join("and")
+console.log(c, typeof c)
+//pop method
+let r=num.pop()
+  console.log(num, r)
+//push method
+let q=num.push()
+  console.log(num, q)
+
+//shift method
+let t= num.shift()
+console.log(t, num)//removes element from start of the array
+
+
+let r= num.unshift(78)// run this method separately for output
+console.log(r, num) 
