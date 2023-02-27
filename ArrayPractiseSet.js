e7715e81522dfce3d2689918295f95454a737015
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-02-27 21:12:50 +0545

    Update ArrayPractiseSet.js

--- ArrayPractiseSet.js
+++ ArrayPractiseSet.js
@@ -7,3 +7,15 @@ let a= prompt("enter a number")
 a =Number.parseInt(a)
 arr.push(a)
 console.log(arr)
+
+//problem2 "keep adding numbers to the array in Q1 until 0 is addd to array
+
+let arr =[1, 2, 3, 4, 5, 6, 7, 83]
+let a;
+do {
+ a = prompt("enter a number")
+
+ a =Number.parseInt(a)
+arr.push(a)
+}while (a != 0);
+console.log(arr)
