d97abb9998cd4a61d582a15eeb2ee164f3f9d839
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-02-27 21:12:19 +0545

    Create ArrayPractiseSet.js

---
+++ ArrayPractiseSet.js
@@ -0,0 +1,9 @@
+// arrays practise set
+
+
+//problem 1 "create an arrray of numbers and take input from users and add the number to array"
+let arr =[1, 2, 3, 4, 5, 6, 7, 83]
+let a= prompt("enter a number")
+a =Number.parseInt(a)
+arr.push(a)
+console.log(arr)
