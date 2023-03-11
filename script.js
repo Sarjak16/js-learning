433fb8cce68e2bd04cb40d427fcd4520af5054ad
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-03-11 12:22:37 +0000

    Question no 4

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red"
 document.getElementsByTagName("nav")[0].firstElementChild.style.color = "green"
 document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green"
 
+//Q4:
+
+Array.from(document.getElementsByTagName("li")).forEach((element) => {
+  element.style.background = "red";
+})
