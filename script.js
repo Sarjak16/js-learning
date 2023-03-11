59568c930b8329352578fecc2e5c068e225a6782
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-03-11 12:21:22 +0000

    Question no 3

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.getElementsByTagName(".nav")[0].firstElementChild.style.color = "red"
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red"
+
+//Q3:
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "green"
+document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green"
+
