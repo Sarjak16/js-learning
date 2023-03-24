1314560aa83a96db8e76d62b2b6a3afdca68f281
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-03-24 16:45:49 +0000

    more attributes

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+let first = document.getElementById("first");
+
 let a = first.getAttribute("class")
 console.log(a)
 console.log(first.hasAttribute("class"))
 console.log(first.hasAttribute("style"))
+//first.setAttribute("hidden", "true")
+first.setAttribute("class", "true truth")
+//first.removeAttribute("class") removes class 
+console.log(first.attributes)//Nodemap
