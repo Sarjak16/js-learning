e14e60e9d70b0712c28f831d6308038d9fba78d0
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-03-28 15:53:55 +0000

    afterbegin

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ first.insertAdjacentHTML('beforebegin', '<div class="test">beforebegin</div>');
 
 first.insertAdjacentHTML('beforeend', '<div class="test">beforeend</div>');
 
+first.insertAdjacentHTML('afterbegin', '<div class="test">afterbegin</div>');
 
 
