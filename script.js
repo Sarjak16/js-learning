793fba4a516c085d154426fcd57d227c7804763a
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-03-28 15:57:09 +0000

    all

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ first.insertAdjacentHTML('beforeend', '<div class="test">beforeend</div>');
 
 first.insertAdjacentHTML('afterbegin', '<div class="test">afterbegin</div>');
 
+first.insertAdjacentHTML('afterend', '<div class="test">afterend</div>');
 
+//remove node
+//remove(first) 
+//this removes all contents of id first
