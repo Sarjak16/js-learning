f7aa0e4b66d9e95b95249d6a0040a3357e47b982
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-04-05 16:08:34 +0000

    set timeout

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-alert("hello")
+document.write("hello")
 
-let a= setTimeout(function(){
-  alert("I am inside set timeout")
-},5000)
+const sum =(a,b,c) =>{
+  console.log("yes i am running"+(a+b+c))
+a+b+c}
+
+
+setTimeout(sum,100,1,2,7)
+
+
+// let a = setTimeout(function() {
+//   alert("I am inside set timeout")
+// }, 5000)
+// let b = prompt("do you want to run set timeout??")
+// if ("no" == b) {
+//   clearTimeout(a)
+// }
